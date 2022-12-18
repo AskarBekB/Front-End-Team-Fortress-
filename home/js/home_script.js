7e482307bc61e2cd12cb4b5bdf5d7541067370af
RamazanMamyrbek <rama.mamirbek@gmail.com>
2022-12-18 15:38:31 +0600

    roma update

--- home/js/home_script.js
+++ home/js/home_script.js
@@ -17,4 +17,56 @@ menu.onclick = () => {
 	navbar.classList.toggle('open');
 }
 
+let slides = document.querySelectorAll('.container-slides');
+let index = 0;
+
+function next(){
+	slides[index].classList.remove('active');
+	index = (index+1)%slides.length;
+	slides[index].classList.add('active');
+
+}
+function prev(){
+	slides[index].classList.remove('active');
+	index = (index-1 + slides.length)%slides.length;
+	slides[index].classList.add('active');
+
+}
+
+
+function video1(){
+	var video1 = document.getElementById('video1');
+	video1.style.visibility = 'visible';
+}
+function close_video1(){
+	var video1 = document.getElementById('video1');
+	video1.style.visibility = 'hidden';
+}
+
+function video2(){
+	var video1 = document.getElementById('video2');
+	video1.style.visibility = 'visible';
+}
+function close_video2(){
+	var video1 = document.getElementById('video2');
+	video1.style.visibility = 'hidden';
+}
+
+function video3(){
+	var video1 = document.getElementById('video3');
+	video1.style.visibility = 'visible';
+}
+function close_video3(){
+	var video1 = document.getElementById('video3');
+	video1.style.visibility = 'hidden';
+}
+
+function video4(){
+	var video1 = document.getElementById('video4');
+	video1.style.visibility = 'visible';
+}
+function close_video4(){
+	var video1 = document.getElementById('video4');
+	video1.style.visibility = 'hidden';
+}
 
